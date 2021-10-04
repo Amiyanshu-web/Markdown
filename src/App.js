964bb915837acbd7e9443f77d0f56dae1057e24b
Amiyanshu-web <59080551+Amiyanshu-web@users.noreply.github.com>
2021-10-04 16:33:55 +0530

    readme updated

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import ReactMarkdown from 'react-markdown'
 function App() {
   const [markdown, setMarkdown] = useState('## markdown preview')
   return (<main>
+    
     <section className="markdown">
       <textarea className='input' value={markdown} onChange={(e)=>setMarkdown(e.target.value)}></textarea>
     <article className="result">
