122da5db08648e4b3e208bf2a13a10fa082e7d19
Yash Gupta <yashg299@gmail.com>
2021-10-08 11:50:53 +0530

    Imporved CSS

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import React, { useState } from 'react'
-import ReactMarkdown from 'react-markdown'
+import React, { useState } from "react";
+import ReactMarkdown from "react-markdown";
 //functional component
 function App() {
-  const [markdown, setMarkdown] = useState('## markdown preview')
-  return (<main>
-    
-    <section className="markdown">
-      <textarea className='input' value={markdown} onChange={(e)=>setMarkdown(e.target.value)}></textarea>
-    <article className="result">
-      <ReactMarkdown>{markdown}</ReactMarkdown>
-      </article>
-    </section>
-  </main>
-  )}
+  const [markdown, setMarkdown] = useState("## markdown preview");
+  return (
+    <main>
+      <section className="markdown">
+        <div className="markdown_editor">
+          <div className="editor_heading">
+            Markdown Editor
+          </div>
+          <textarea
+            className="input"
+            value={markdown}
+            onChange={(e) => setMarkdown(e.target.value)}
+            rows='10'
+          ></textarea>
+        </div>
+        <article className="markdown_result">
+        <div className="editor_heading">
+            Markdown Result
+          </div>
+          <ReactMarkdown>{markdown}</ReactMarkdown>
+        </article>
+      </section>
+    </main>
+  );
+}
 
-export default App
+export default App;
